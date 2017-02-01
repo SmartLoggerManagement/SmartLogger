b91ff4ab6ab325f5e375f51ed1d2f4c83e3cc566
Jordan Baudin <madzinah@gmail.com>
2017-02-01 16:28:46 +0100

    Changement définition IAnalyzer

--- smartlogger/src/main/scala/ML/IAnalyzer.scala
+++ smartlogger/src/main/scala/ML/IAnalyzer.scala
@@ -2,11 +2,12 @@ package scala.ML
 
 trait IAnalyzer {
 
-  def train (data: Seq[String]): Unit
+    def train (data: Seq[(Long, String, Double)]): Unit
 
-  def predict (data: Seq[String]): Unit
+    def predict (data: Seq[(Long, String)]): Seq[(Long, String, Double)]
 
-  def getWeights(): Seq[Any]
+    def getWeights: String
 
-  def setWeights(data: Seq[Any]): Unit
+    def setWeights(data: Seq[Any]): Unit
+
 }
