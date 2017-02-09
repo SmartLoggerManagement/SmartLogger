593c9db1b0b40713ee3d4ceca1e1102034724378
KittyPwa <c.leplumey@gmail.com>
2017-02-09 11:43:22 +0100

    SmartAnalyser Tests V0.0.1 - first four tests implemented

--- smartlogger/src/test/scala/IAnalysor/SmartAnalyzerTest.scala
+++ smartlogger/src/test/scala/IAnalysor/SmartAnalyzerTest.scala
@@ -2,12 +2,61 @@ import org.scalatest.FunSuite
 
 class SmartAnalyzerTest extends FunSuite {
 
-    test("testTrain") {
-
+    /* testing train(null)
+    * expected behaviour : illegal argument exception
+    */
+    test("testTrainNull") {
+      val analy = new SmartAnalyzer(new NaiveBayes)
+        try {
+          analy.test(null)
+          assert(false)
+        } catch IllegalArgumentException Exception {
+          assert(true)
+        }
     }
 
-    test("testPredict") {
-
+    /*Testing train(seq)
+    *expected behavior : old.analy.model != new.analy.model
+     */
+    test("testTrainChange") {
+      val analy = new SmartAnalyzer(new NaiveBayes)
+      val seq = Seq.apply(
+        (0L, "X totally sucks :-(", 0.0),
+        (1L, "Today was kind of meh", 1.0),
+        (2L, "I'm so happy :-)", 2.0)
+      )
+      val oldModel = analy.model
+      analy.train(seq)
+      assert(oldModel != analy.model)
     }
 
+    /*Testing predict(null)
+    *expected behavior : IllegalArgumentException
+     */
+    test("testPredictNull") {
+      val analy = new SmartAnalyzer(new NaiveBayes)
+      try {
+        analy.predict(null)
+        assert(false)
+      } catch IllegalArgumentException Exception {
+        assert(true)
+      }
+    }
+
+    /*Testing predict(seq) giving always the same result
+    *expected behavior : predict(seq) == predict(seq)
+     */
+    test("testPredictSameRes") {
+      val analy = new SmartAnalyzer(new NaiveBayes)
+      val seq = Seq.apply(
+        (4L, "roller coasters are fun :-)"),
+        (5L, "i burned my bacon :-("),
+        (6L, "the movie is kind of meh")
+      )
+      assert(analy.predict(seq) == analy.predict(seq))
+    }
+
+    /*
+    *
+     */
 }
