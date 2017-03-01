3815e405d866a20f9c2b926934a6a3539fa04ab1
Jordan Baudin <madzinah@gmail.com>
2017-03-01 18:19:24 +0100

    Began MailerSender & moved benchmark to avoid high-cost tests each time

--- smartlogger/src/main/scala/io/output/INotifier.scala
+++ smartlogger/src/main/scala/io/output/INotifier.scala
@@ -1,5 +1,7 @@
 package output
 
+import javax.mail.MessagingException
+
 /**
   * Created by Kero76 on 15/02/17.
   */
@@ -8,10 +10,11 @@ trait INotifier {
   /**
     * Send the message into a communication flux.
     *
-    * @param content
+    * @param text
     *   Content to send.
+    *   @throws MessagingException
     * @since SmartLogger 1.0
     * @version 1.0
     */
-  def send(content: String) : Unit
+  def send(text: String) : Unit
 }
--- smartlogger/src/main/scala/io/output/MailerSender.scala
+++ smartlogger/src/main/scala/io/output/MailerSender.scala
@@ -1,43 +1,43 @@
 package output
 
-import courier.{Envelope, Mailer, Text}
-import javax.mail.internet.InternetAddress
-import scala.concurrent.ExecutionContext.Implicits.global
+import java.util.Properties
+import javax.mail.internet.{InternetAddress, MimeMessage}
+import javax.mail.Message.RecipientType
+import javax.mail.{Session, Transport}
 
-import scala.util.{Failure, Success, Try}
-
-/**
-  * Created by Kero76 on 15/02/17.
+/** MailerSender constructor.
+  *  @constructor MailerSender
+  *
+  * @param serverAddr
+  *    ServerAdress
+  * @param from
+  *    Sender.
+  * @param tos
+  *    Receivers.
+  * @param ccs
+  *    CCs receivers.
+  * @param subject
+  *    Subject to send.
+  *
   */
-class MailSender(serverAddr: String, port: Int, mailAddr: String, pwd: String) extends INotifier {
+class MailerSender(serverAddr: String, from: String, tos: Seq[String], ccs: Seq[String], subject: String) extends INotifier {
+
+  val session = Session.getDefaultInstance(new Properties() { put("mail.smtp.host", serverAddr) })
 
   /**
-    * Stored information about mail contact
+    * @inheritdoc
+    * @param text
+    *   Content to send.
     */
-  var _contactInformation : Seq[String] = Seq.empty
-
-  /**
-    *
-    * @param content
-    * Content to send.
-    * @since SmartLogger 1.0
-    * @version 1.0
-    */
-  override def send(content: String): Unit = {
-    val mailer = Mailer(serverAddr, port)
-      .auth(true)
-      .as(mailAddr, pwd)
-      .startTtls(true)()
-
-    mailer(Envelope.from(new InternetAddress("you@gmail.com"))
-      .to(new InternetAddress("mom@gmail.com"))
-      .cc(new InternetAddress("dad@gmail.com"))
-      .subject("Error on your server")
-      .content(Text(content))).onComplete {
-        case Success(message) => println("Message delivered : " + message)
-        case Failure(message) => println("Failure during the sending of the message : "
-            + message)
-    }
-
+  def send(text: String) {
+    val message = new MimeMessage(session)
+    message.setFrom(new InternetAddress(from))
+    for (to <- tos)
+      message.addRecipient(RecipientType.TO, new InternetAddress(to))
+    for (cc <- ccs)
+      message.addRecipient(RecipientType.TO, new InternetAddress(cc))
+    message.setSubject(subject)
+    message.setText(text)
+    Transport.send(message)
   }
 }
