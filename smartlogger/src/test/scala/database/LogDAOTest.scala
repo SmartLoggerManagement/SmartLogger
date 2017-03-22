e220310faa905d1ad99694c52a754b250d86801d
KittyPwa <c.leplumey@gmail.com>
2017-03-22 11:32:23 +0100

    LogDao Test

--- smartlogger/src/test/scala/database/LogDAOTest.scala
+++ smartlogger/src/test/scala/database/LogDAOTest.scala
@@ -22,20 +22,25 @@ class LogDAOTest extends FeatureSpec with Matchers with GivenWhenThen {
   feature("The Database executes the query and fills the sequence 'result' with the resulting logs.") {
     info("To simulate the database's behaviour, we will mock its responses.")
 
-    scenario("A querry is sent to the database. The database responds with the appropriate answer.") {
-      Given("Create a string querry.")
-      val uuid = UUID.randomUUID();
-      val query : String = "SELECT Log from Logs where id =" + uuid.toString()
+    val uuid = UUID.randomUUID();
 
-      val resultSetMock = MockitoSugar.mock[ResultSet]
-      Mockito.when(resultSetMock.getString("id")).thenReturn(uuid.toString())
-      Mockito.when(resultSetMock.getString("log")).thenReturn("I am correct Log")
+    scenario("A create log query is sent to the database.") {
+      Given("Create the 'set' query with the message")
+      val message = "I am correct log"
+      val insertQuery : String = "Insert into Logs values (" + uuid + ", " + message + ")"
 
-      val DBMock = MockitoSugar.mock[DbConnector.type]
-      Mockito.when(DBMock.openConnection.createStatement.executeQuery("SELECT * from Logs where id = " + uuid.toString())).
-        thenReturn(resultSetMock)
+      When("Query the Database with the query")
+      LogDAO.execute(insertQuery)
 
-      When("Querry the Database with the querry and analyse the result")
+      Then("Check that the query passed successfully")
+      assert(true)
+    }
+
+    scenario("A get query is sent to the database. The database responds with the appropriate answer.") {
+      Given("Create the string 'get' query.")
+      val query : String = "Select * from Logs where id = " + uuid + ")"
+
+      When("Query the Database with the query and analyse the result")
       val result = LogDAO.query(query)
 
       Then("The log id must be equal to uuid")
@@ -44,5 +49,26 @@ class LogDAOTest extends FeatureSpec with Matchers with GivenWhenThen {
       And("the message must be equal to 'I am correct Log'")
       result.head.getLog.toString() should equal("I am correct Log")
     }
+
+    scenario("A get query is sent to the database with an invalid uuid. The database responds with the appropriate answer.") {
+      Given("Create the invalid uuid and the 'get' query")
+      val invalidUuid = UUID.randomUUID()
+      val query : String = "Select * from Logs where id = " + invalidUuid + ")"
+
+      When("Query the Database with the query and analyse the result")
+      val result = LogDAO.query(query)
+
+      Then("The log id must launch an exception")
+      intercept[Exception] {
+        result.head.getId
+      }
+
+      And("The log message must launch an exception")
+      intercept[Exception] {
+        result.head.getLog
+      }
+    }
+
+    scenario()
   }
 }
